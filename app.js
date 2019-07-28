d4aafc2b68993acf5c5bf3369d69192d3121debe
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-07-29 00:22:43 +0600

    added config

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('dev'))
 
+const { username, password, host, dbname, dbengine } = require('./utils/config');
+const mongoUri =  dbengine + '://' +  username + ':' +  password + '@' +  host + '/' +  dbname;
+
+app.use(morgan('dev'))
 app.use(express.json())
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
 })
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log('Server started at port '+port)
