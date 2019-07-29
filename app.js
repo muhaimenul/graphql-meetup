fbb3b9b23e2cb3f227473e3a2d94c137fde474f3
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-07-30 00:46:17 +0600

    added dummy Event query type and input type

--- app.js
+++ app.js
@@ -11,15 +11,56 @@ const mongoUri =  dbengine + '://' +  username + ':' +  password + '@' +  host +
 
 app.use(morgan('dev'))
 app.use(express.json())
+const events = [];
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
         schema {
-            query:
-            mutation:
+            query: RootQuery
+            mutation: RootMutation
         }
     `),
-    rootValue: {}
-}));
+    rootValue: {
+        events: () => {        
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+              };
+              events.push(event);
+              return event;
+        }
+    },
+    graphiql: true
+})
+);
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
