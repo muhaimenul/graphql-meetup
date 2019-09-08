3074d7b0ec3c0bd424ae0b6788682c8694340c32
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-09-08 22:46:35 +0600

    setup routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" component={null} />
+        <Route path="/auth" component={null} />
+        <Route path="/events" component={null} />
+        <Route path="/bookings" component={null} />
+      </Router>
+    )
+  }
 }
-
-export default App;
