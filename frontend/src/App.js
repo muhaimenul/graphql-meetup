c6b95a783f8778967cb5269397062b6e3b5500f4
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-09-09 00:27:12 +0600

    inlcuded navigation on app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './routes'
 
+import MainNavigation from './components/Navigation/MainNavigation'
+
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <Routes />
+        <MainNavigation />
+        <main>
+          <Routes />
+        </main>
       </Router>
     )
   }
